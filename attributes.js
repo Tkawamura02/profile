e06d377920e9932a73bc0c835f884070708e6abe
Tiffany Kawamura <86638759+Tkawamura02@users.noreply.github.com>
2022-12-10 17:22:36 -0700

    Changed typewriter

--- attributes.js
+++ attributes.js
@@ -1,3 +1,7 @@
+var quoteNum = 0;
+var speed = 50;
+var i = 0;
+
 const textField = document.getElementById('open');
 const typewriter = new Typewriter(textField,{
   loop: false,
@@ -11,4 +15,36 @@ typewriter
   .typeString(`<span>Reader!</span`)
   .pauseFor(300)
   .typeString(`<span> My name is Tiffany Kawamura.</span`)
+  .cursor(transparent)
   .start()
+
+var quotes = [
+  'Hello World! My name is Tiffany Kawamura.',
+  'What is your name?'
+];
+
+$(document).ready(function() {
+    $("#nameButton").click(function() {
+      $("#nameButton").fadeOut()
+      $("#userName").fadeIn()
+      const typewriterName = new Typewriter(document.getElementById(0),{
+        loop: false,
+        delay: 75,
+      })
+
+      typewriterName
+        .typeString(`<span>Hello World!</span>`)
+    })
+});
+
+/*
+function typeWriter() {
+  var txt = quotes[quoteNum];
+  if (i<txt.length) {
+    document.getElementById(quoteNum).innerHTML += txt.charAt(i);
+    i++;
+  }
+  setTimeout(typeWriter, speed);
+  quoteNum++;
+}
+*/
